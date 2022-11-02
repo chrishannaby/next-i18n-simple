ad0de09b0270b743d769c54c2027401707f3f024
Chris Hannaby <chris.hannaby@netlify.com>
2022-11-02 12:49:04 -0600

    try i18n domains

--- next.config.js
+++ next.config.js
@@ -2,5 +2,18 @@ module.exports = {
   i18n: {
     locales: ["en", "fr", "de"],
     defaultLocale: "en",
+
+    domains: [
+      {
+        // Note: subdomains must be included in the domain value to be matched
+        // e.g. www.example.com should be used if that is the expected hostname
+        domain: "example.com",
+        defaultLocale: "en",
+      },
+      {
+        domain: "example.fr",
+        defaultLocale: "fr",
+      },
+    ],
   },
 };
